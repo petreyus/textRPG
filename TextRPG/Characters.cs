37844ab20aeb17bc4809bd43228f82175182438c
Petreyus <54726563+petreyus@users.noreply.github.com>
2020-11-03 14:25:35 -0600

    Add files via upload

---
+++ TextRPG/Characters.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace PlayerClass
+{
+
+    public class Character
+    {
+
+      
+        public static string Name { get; set; } = "";
+        public static string Class { get; set; } = "";
+        // private int Attack { get; set; } = rnd.Next(1, 100);
+     
+            
+
+            public static void pickClass()
+        {
+
+             string userInput = Console.ReadLine();
+            // if user selects anything but 1,2,3
+            // call function again: take it from the top
+            switch (userInput)
+            {
+                case "1":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You are a warrior!");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Character.Class="warrior";
+                    break;
+
+                case "2":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("You are a mage!");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Character.Class = "mage";
+                    break;
+                case "3":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("You are a thief!");
+                    Character.Class = "thief";
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Please type a number 1-3 to select your class");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    pickClass();
+                    break;
+
+            }
+
+
+
+
+        }
+
+
+       public void ATK()
+        {
+            Random rnd = new Random();
+            int Attack = rnd.Next(1, 100);
+            Console.WriteLine("your attack is:"+ @Attack);
+        }
+
+
+       public static void HP()
+        {
+            Console.WriteLine("Input your health");
+            string hp = Console.ReadLine();
+            Console.WriteLine("your hp is:" + hp);
+         }
+
+
+
+
+
+
+    }
+
+
+}
+
+
+
+
---
+++ TextRPG/ConsoleInputStringValidator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace NameCheck
+{
+    class StringValidator
+    {
+
+        public static string ConsoleUserInputChecker(string Name)
+
+        {
+            // checks for any digits in the input string
+            foreach (char c in Name)
+
+             if (!Char.IsLetter(c))
+                {
+
+
+                    Console.WriteLine("Input must be letters only; try again.");
+                    Name = Console.ReadLine();
+                    return ConsoleUserInputChecker(Name);
+                }
+            // checks for empty input string
+            if (string.IsNullOrEmpty(Name))
+
+            {
+
+                Console.WriteLine("Input can't be empty; try again.");
+                Name = Console.ReadLine();
+                return ConsoleUserInputChecker(Name);
+            }
+            // To do:
+            // check if user has a Space in name, 
+            // then output error as "no character spaces".
+
+            else
+            {
+
+
+                return (Name);
+            }
+
+
+
+        }
+
+
+    }
+}
